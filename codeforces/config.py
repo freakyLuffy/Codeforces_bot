1fd81051ecd6bd8ccbcd6641668b4d543a0abf82
freakyLuffy <63852333+freakyLuffy@users.noreply.github.com>
2024-05-27 20:35:52 +0530

    Update config.py

--- codeforces/config.py
+++ codeforces/config.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
+from dotenv import load_dotenv
 
+load_dotenv()
 # Telegram Bot Token
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', '')
 LOG_CHANNEL=os.getenv('LOG_CHANNEL','')
