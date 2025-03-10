3563fbce81b630e4ffeae3675699a8f9d93f950d
Hima <63852333+freakyLuffy@users.noreply.github.com>
2025-03-10 22:11:13 +0530

    Update __main__.py

--- codeforces/__main__.py
+++ codeforces/__main__.py
@@ -8,7 +8,7 @@ from codeforces.modules import ALL_MODULES
 
 
 
-print(ALL_MODULES)
+
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("codeforces.modules." + module_name)
 
